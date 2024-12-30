619b2480f441fad62b1cb1e2b08251d3b743b3f5
Ritesh Gupta <riteshgupta0968@gmail.com>
2024-12-30 15:46:19 +0530

    Configured Vite proxy for backend on port 5000

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', 
+    outDir: 'dist', // The output directory for build
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000', // Proxy API requests to backend on port 5001
+    },
   },
 });
